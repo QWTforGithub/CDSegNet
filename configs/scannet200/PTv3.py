5c99ec0d10226a32c9a6c22cd98550346ee2310b
QWTforGithub <83633231+QWTforGithub@users.noreply.github.com>
2025-03-09 09:29:17 +0800

    Update PTv3.py

--- configs/scannet200/PTv3.py
+++ configs/scannet200/PTv3.py
@@ -143,7 +143,7 @@ optimizer = dict(type="AdamW", lr=0.006, weight_decay=0.05)
 scheduler = dict(
     type="OneCycleLR",
     max_lr=[0.006, 0.0006],
-    pct_start=0.50,
+    pct_start=0.05,
     anneal_strategy="cos",
     div_factor=10.0,
     final_div_factor=1000.0,
